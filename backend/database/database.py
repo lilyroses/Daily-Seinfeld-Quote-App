479853167c43e298c0888a28d0ffaf34cb4fd4bc
lilyroses <lilyrosestracke.dev@gmail.com>
2024-02-11 00:41:02 -0800

    Update app database

--- backend/database/database.py
+++ backend/database/database.py
@@ -11,8 +11,7 @@ c = conn.cursor()
 #           season_no int,
 #           air_date_start text,
 #           air_date_end text
-#       )"""
-# )
+#           )""")
 
 # c.execute(
 #     """CREATE TABLE episodes (
@@ -22,15 +21,30 @@ c = conn.cursor()
 #           chronological_no int,
 #           title text,
 #           air_date text
-#         )"""
+#           )""")
+
+# c.execute(
+#     """CREATE TABLE quote (
+#           quote_id int,
+#           quotee_id int,
+#           episode_id int,
+#           quote_text text
+#           )""")
+
+# c.execute(
+#     """CREATE TABLE quotee (
+#           character_id int,
+#           quote_id int,
+#           image blob,
+#           PRIMARY KEY (character_id, quote_id)
+#           )"""
 # )
 
 c.execute(
-    """CREATE TABLE quote (
-          quote_id int,
-          quotee_id int,
-          episode_id int,
-          quote_text text
+    """CREATE TABLE characters (
+          character_id int,
+          name text,
+          image blob
 )"""
 )
 
